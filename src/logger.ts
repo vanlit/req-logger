07b28fffd40c232ba7350d551f755a96f6989de0
i2.ilin <i2.ilin@itransition.com>
2019-04-01 18:58:56 +0300

    pour the working code finally outta the working area

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,5 @@
 import * as fse from 'fs-extra';
+import * as fs from 'fs';
 
 export interface ICategorizedDataLogger {
     Log: (category: string, data: any) => Promise<void>
@@ -13,16 +14,45 @@ export default class CategorizedDataLogger implements ICategorizedDataLogger{
     }
 
     public Log(category: string, data: any): Promise<void> {
-        return fse.open(`${this._logDir}/${category}.log`, 'wa')
-        .then( (fDesc)=> {
+        const filename = `${this._logDir}/${category}.log`;
+        return fse.open(filename, 'a')
+        .then( (fDesc: number)=> {
             return fse.appendFile(
                 fDesc,
-                `${new Date().toISOString()}: ${JSON.stringify(data)}\n`
+                `${new Date().toISOString()}: ${JSON.stringify(data)}\r\n`
             )
             .then( () => {
                 return fse.close(fDesc);
-            })
+            });
         });
+    };
+
+    public GetLog(category: string, buffer: Buffer, byteSizeLimit: number = 1024*100): Promise<string> {
+        const filename = `${this._logDir}/${category}.log`;
+
+        return new Promise<string>((resolveLogGetting, rejectLogGetting) => {
+            const fileLen = fs.stat(filename, (err, filestats) => {
+                if(err) {rejectLogGetting(err);}
+                else {
+                    const offset = (filestats.size - byteSizeLimit > 0) ? filestats.size - byteSizeLimit : 0;
+                    fse.open(filename, 'ro')
+                    .then( (fDesc: number)=> {
+                        fse.read(
+                            fDesc, buffer, offset, byteSizeLimit, 0
+                        )
+                        .then( () => {
+                            fse.close(fDesc);
+                            resolveLogGetting();
+                        })
+                        .catch((err) => {
+                            fse.close(fDesc);
+                            rejectLogGetting(err);
+                        });
+                    });
+                }
+            });
+        })
+
     }
 
 }
--- src/routes.ts
+++ src/routes.ts
@@ -11,11 +11,25 @@ export function registerLoggerRoutes(app: Express, logger: CategorizedDataLogger
             res.status(200).send();
         })
         .catch( (err) => {
-            console.log(JSON.stringify(err));
+            console.log(err);
             res.status(500).send(JSON.stringify(err));
-        })
+        });
     });
 
-    app.get('/log/:category')
+    app.get('/log/:category', (req: Request, res: Response) => {
+        const catg = req.params['category'];
+        console.log(`Returning the whole "${catg}" category.`);
+
+        const byteSizeLimit: number = 1024*100
+        const buff = Buffer.allocUnsafe(byteSizeLimit);
+        logger.GetLog(catg, buff, byteSizeLimit)
+        .then(()=>{
+            res.send(buff);
+        })
+        .catch((err)=>{
+            res.status(500);
+            res.send(JSON.stringify(err));
+        });
+    });
 }
 
